fd87f56cfc61091ac7fe4f86b5c6c9d00b5ae3ff
Michael Thompson <michael.220488@gmail.com>
2019-10-19 18:43:25 -0700

    change jsonp to json

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+      
+    }
+  }
+  render() {
+    return(
+      <div>
+        Map
+      </div>
+    )
+  }
+}
+
+export default App
